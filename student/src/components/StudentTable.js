501ebac5b93e20a70c9deb5ced8b911125dea478
Sanduni0601 <139616844+Sanduni0601@users.noreply.github.com>
2024-11-18 13:00:13 +0000

    create StudentTable component

---
+++ student/src/components/StudentTable.js
@@ -0,0 +1,33 @@
+import '../assests/css/compo.css';
+export default function StudentTable(props){
+    return(
+        <div className = "outerDiv">
+             <div className="leftDiv">
+                <table border="1">
+                    <thead>
+                        <tr>
+                            <td>First Name</td>
+                            <td>Last Name</td>
+                            <td>Course</td>
+                            <td>Country</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            props.students.map( student=>
+                                <tr>
+                                    <td>{student.firstName}</td>
+                                    <td>{student.lastName}</td>
+                                    <td>{student.course}</td>
+                                    <td>{student.address.country}</td>
+                                </tr>
+
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div>
+             
+        </div>
+    );
+}
